142bde49bde6e16bea805f9bdfde6468eaafe545
Matic Leva <vertical3life@gmail.com>
2018-04-08 12:27:46 +0200

    Create dirt+grass tiles and remove unused tiles (mostly objects that are going to be used directly as sprites)

--- Assets/Tilemap/Tiles/Random Tile/Scripts/RandomTile.cs
+++ Assets/Tilemap/Tiles/Random Tile/Scripts/RandomTile.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -11,9 +12,11 @@ using UnityEngine;
 namespace UnityEngine.Tilemaps
 {
 	[Serializable]
-	public class RandomTile : Tile
-	{
-		[SerializeField]
+    [CreateAssetMenu(fileName = "AnimatedTile", menuName = "Tiles/Random Tile", order = 3)]
+    public class RandomTile : Tile
+    {
+        public Sprite m_DefaultSprite;
+        [SerializeField]
 		public Sprite[] m_Sprites;
 
 		public override void GetTileData(Vector3Int location, ITilemap tileMap, ref TileData tileData)
@@ -31,19 +34,6 @@ namespace UnityEngine.Tilemaps
 				tileData.sprite = m_Sprites[(int) (m_Sprites.Length * Random.value)];
 			}
 		}
-
-#if UNITY_EDITOR
-		[MenuItem("Assets/Create/Random Tile")]
-		public static void CreateRandomTile()
-		{
-			string path = EditorUtility.SaveFilePanelInProject("Save Random Tile", "New Random Tile", "asset", "Save Random Tile", "Assets");
-
-			if (path == "")
-				return;
-
-			AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<RandomTile>(), path);
-		}
-#endif
 	}
 
 #if UNITY_EDITOR
@@ -53,8 +43,9 @@ namespace UnityEngine.Tilemaps
 		private RandomTile tile { get { return (target as RandomTile); } }
 
 		public override void OnInspectorGUI()
-		{
-			EditorGUI.BeginChangeCheck();
+        {
+            EditorGUI.BeginChangeCheck();
+            tile.m_DefaultSprite = EditorGUILayout.ObjectField("Default Sprite", tile.m_DefaultSprite, typeof(Sprite), false) as Sprite;
 			int count = EditorGUILayout.DelayedIntField("Number of Sprites", tile.m_Sprites != null ? tile.m_Sprites.Length : 0);
 			if (count < 0)
 				count = 0;
@@ -75,7 +66,58 @@ namespace UnityEngine.Tilemaps
 			}		
 			if (EditorGUI.EndChangeCheck())
 				EditorUtility.SetDirty(tile);
-		}
-	}
+        }
+
+        public override Texture2D RenderStaticPreview(string assetPath, Object[] subAssets, int width, int height)
+        {
+            if (tile.m_DefaultSprite != null)
+            {
+                Type t = GetType("UnityEditor.SpriteUtility");
+                if (t != null)
+                {
+                    MethodInfo method = t.GetMethod("RenderStaticPreview", new Type[] { typeof(Sprite), typeof(Color), typeof(int), typeof(int) });
+                    if (method != null)
+                    {
+                        object ret = method.Invoke("RenderStaticPreview", new object[] { tile.m_DefaultSprite, Color.white, width, height });
+                        if (ret is Texture2D)
+                            return ret as Texture2D;
+                    }
+                }
+            }
+            return base.RenderStaticPreview(assetPath, subAssets, width, height);
+        }
+
+        private static Type GetType(string TypeName)
+        {
+            var type = Type.GetType(TypeName);
+            if (type != null)
+                return type;
+
+            if (TypeName.Contains("."))
+            {
+                var assemblyName = TypeName.Substring(0, TypeName.IndexOf('.'));
+                var assembly = Assembly.Load(assemblyName);
+                if (assembly == null)
+                    return null;
+                type = assembly.GetType(TypeName);
+                if (type != null)
+                    return type;
+            }
+
+            var currentAssembly = Assembly.GetExecutingAssembly();
+            var referencedAssemblies = currentAssembly.GetReferencedAssemblies();
+            foreach (var assemblyName in referencedAssemblies)
+            {
+                var assembly = Assembly.Load(assemblyName);
+                if (assembly != null)
+                {
+                    type = assembly.GetType(TypeName);
+                    if (type != null)
+                        return type;
+                }
+            }
+            return null;
+        }
+    }
 #endif
 }
